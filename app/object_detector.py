60d7266b17a53f24bb1c95edc29fe46ee45f1b27
nikodemicek <56070150+nikodemicek@users.noreply.github.com>
2024-03-05 11:15:24 +0100

    larger model

--- app/object_detector.py
+++ app/object_detector.py
@@ -8,9 +8,9 @@ def get_model_weights():
     cfg = get_cfg()
     cfg.MODEL.DEVICE = "cpu"
 
-    cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+    cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_X_101_32x8d_FPN_3x.yaml"))
     cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.6  # set threshold for this model
-    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
+    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_X_101_32x8d_FPN_3x.yaml")
 
     # Initialize the predictor with the configuration
     predictor = DefaultPredictor(cfg)
